ad870023ed89e0a7c0b021b13e232adf836c93d3
Venkat Koduri <vkoduri@Stenograph.com>
2022-01-19 08:58:49 -0600

    Pushed Api changes

---
+++ FreeWheels.Api/Controllers/MovieController.cs
@@ -0,0 +1,105 @@
+﻿using Freewheels.App.Interfaces;
+using Freewheels.App.Model.DataContext;
+using Freewheels.App.Model.Dto;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeWheels.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+
+        private readonly ILogger<MovieController> _logger;
+
+        private readonly MovieDataContext _context;
+
+        private readonly IMovie _movie;
+
+        public MovieController(ILogger<MovieController> logger, MovieDataContext context, IMovie movie)
+        {
+            _logger = logger;
+            _context = context;
+            _movie = movie;
+        }
+
+        [HttpGet]
+        [Route("GetUsers")]
+        public IActionResult GetUsers()
+        {
+            var allUsers = _context.Users.ToList();
+
+            return Ok(allUsers);
+        }
+
+        [HttpPost]
+        [Route("AddMovieRating")]
+        public async Task<IActionResult> AddMovieRating([FromBody] RatingDto ratingDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var isRatingAdded = await _movie.AddorUpdateRatingtoaMovie(ratingDto);
+
+                if (!isRatingAdded)
+                {
+                    return NotFound();
+                }
+                return Ok();
+            }
+            return BadRequest();
+        }
+
+        [HttpGet]
+        [Route("GetHighestRatingByUser")]
+        public async Task<IActionResult> GetHighestRatingByUser([FromQuery] string userName)
+        {
+            if (!string.IsNullOrEmpty(userName))
+            {
+                var result = await _movie.GetMoviesBySpecificUserHighestRating(userName);
+
+                if (!result.Any())
+                {
+                    return NotFound();
+                }
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+
+        [HttpGet]
+        [Route("GetAverageRatingForTotalUsers")]
+        public async Task<IActionResult> GetAverageRatingForTotalUsers()
+        {
+            var result = await _movie.GetMoviesByTotalUserAverageRatings();
+            if (!result.Any())
+            {
+                return NotFound();
+            }
+
+            return Ok(result);
+
+        }
+
+        [HttpGet]
+        [Route("GetMoviesBySearchCriteria")]
+        public async Task<IActionResult> GetMoviesBySearchCriteria([FromQuery] SearchDto searchDto)
+        {
+            if (searchDto != null)
+            {
+                var result = await _movie.GetMoviesBySearchCriteria(searchDto);
+
+                if (result.Any())
+                {
+                    return Ok(result);
+                }
+                return NotFound();
+            }
+            return BadRequest();
+        }
+    }
+}
---
+++ FreeWheels.Api/Startup.cs
@@ -0,0 +1,60 @@
+using Freewheels.App.Interfaces;
+using Freewheels.App.Logic;
+using Freewheels.App.Model.DataContext;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreeWheels.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddDbContext<MovieDataContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("MovieDbConnection"));
+            });
+            services.AddScoped<IMovie, MovieLogic>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
